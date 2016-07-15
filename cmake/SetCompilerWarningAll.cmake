c03d789a3002a3c13c085a8fad16d0f97cfa76dc
Sergey Shambir <sergey.shambir.auto@gmail.com>
2016-07-15 09:14:21 +0300

    Настройка предупреждений в Visual Studio
    Включён уровень W4, отключено предупреждение C4201 о нестандартном
    расширении для union/struct.

---
+++ cmake/SetCompilerWarningAll.cmake
@@ -0,0 +1,36 @@
+# - Enable warning all for gcc or use /W4 for visual studio
+
+#
+# TODO:
+# - Make it more portable
+
+## Strict warning level
+IF (MSVC)
+  # Use the highest warning level for visual studio.
+  SET(CMAKE_CXX_WARNING_LEVEL 4)
+  IF (CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+    STRING(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  ELSE (CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
+  ENDIF (CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+
+  SET(CMAKE_C_WARNING_LEVEL 4)
+  IF (CMAKE_C_FLAGS MATCHES "/W[0-4]")
+    STRING(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+  ELSE (CMAKE_C_FLAGS MATCHES "/W[0-4]")
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4")
+  ENDIF (CMAKE_C_FLAGS MATCHES "/W[0-4]")
+
+ELSEIF (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUC)
+  # use -Wall for gcc
+  IF(NOT CMAKE_CXX_FLAGS MATCHES "-Wall$")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+  ENDIF(NOT CMAKE_CXX_FLAGS MATCHES "-Wall$")
+
+  IF(NOT CMAKE_C_FLAGS MATCHES "-Wall$")
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
+  ENDIF(NOT CMAKE_C_FLAGS MATCHES "-Wall$")
+
+ELSE (MSVC)
+  MESSAGE(STATUS "Unknown build tool, cannot set warning flags for your")
+ENDIF (MSVC)
