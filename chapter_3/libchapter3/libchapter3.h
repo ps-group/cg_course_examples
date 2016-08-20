855d8fbf7e93575b3b4287193aa3dfa40485ecdf
Sergey Shambir <sshambir@yandex.ru>
2016-08-20 20:48:54 +0300

    Добавлены забытые h-файлы

---
+++ chapter_3/libchapter3/libchapter3.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "src/libchapter3_private.h"
+#include "src/Window.h"
+#include "src/AbstractWindowClient.h"
+#include "src/Camera.h"
+#include "src/Lights.h"
+
+#include "src/Texture2D.h"
+#include "src/ShaderProgram.h"
