01cfc823052a00fcb5f5f4d64763682534b8fe93
Sergey Shambir <sshambir@yandex.ru>
2016-11-05 14:47:01 +0300

    Добавил пропущенные файлы

---
+++ chapter_4/libs/libscene.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "libscene/ArrayObject.h"
+#include "libscene/BufferObject.h"
+#include "libscene/Texture2D.h"
+
+#include "libscene/Camera.h"
+#include "libscene/Geometry.h"
+#include "libscene/Tesselator.h"
+#include "libscene/Model3D.h"
+#include "libscene/Model3DRenderer.h"
+
+#include "libscene/AssetLoader.h"
+#include "libscene/ModelLoader.h"
---
+++ chapter_4/libs/libsystem.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "libsystem/IInputListener.h"
+#include "libsystem/Window.h"
+#include "libsystem/AbstractWindowClient.h"
+#include "libsystem/Utils.h"
+#include "libsystem/MouseGrabber.h"
+#include "libsystem/PlatformFeatures.h"
